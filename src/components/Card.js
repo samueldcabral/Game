b553b0d8e85a742c02fbede9fb31a9bae9ef5fc9
Renatha <renatha.nvictor@gmail.com>
2019-10-02 15:57:43 -0300

    some modifications

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,20 @@
 import React from 'react'
 
-import Archer from '../img/heros/archer.png';
-
-const Card = () => {
+const Card = ({name, brain, defense, hp, speed, strenght}) => {
   return (
     <div className="card-bg container flex-column">
       <div>
-        <p className="color-black bolder card-name">Archer</p>
+        <p className="color-black bolder card-name">{name}</p>
       </div>
       <div>
-        <img className="card-img" src={Archer} alt="archer"/>
+        <img className="card-img" src={require(`../img/heros/${name.toLowerCase()}.png`)} alt={name}/>
       </div>
       <div className="card-box color-black">
-        <p><strong><i class="fas fa-brain"></i> Brain</strong>: 80</p>
-        <p><strong><i class="fas fa-shield-alt"></i> Defense</strong>: 60</p>
-        <p><strong><i class="fas fa-plus-square"></i> HP</strong>: 90</p>
-        <p><strong><i class="fas fa-tachometer-alt"></i> Speed</strong>: 80</p>
-        <p><strong><i class="fas fa-dumbbell"></i> Strenght</strong>: 70</p>
+        <p><strong><i class="fas fa-brain"></i> Brain</strong>: {brain}</p>
+        <p><strong><i class="fas fa-shield-alt"></i> Defense</strong>: {defense}</p>
+        <p><strong><i class="fas fa-plus-square"></i> HP</strong>: {hp}</p>
+        <p><strong><i class="fas fa-tachometer-alt"></i> Speed</strong>: {speed}</p>
+        <p><strong><i class="fas fa-dumbbell"></i> Strenght</strong>: {strenght}</p>
       </div>
     </div>
   )
