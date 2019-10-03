33ecd480dea4b08a5b6783ff43e2a97cc79ccdcd
Adriano <adriano.amaral147@gmail.com>
2019-10-03 12:07:32 -0300

    pequenas alteraçoes de layout

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,6 +1,7 @@
 import React from 'react'
 // import { Link } from 'react-router-dom'
 import GameButton from './GameButton'
+import logo from '../img/Pergaminho2.jpg'
 
 const Dashboard = props => {
     return (
@@ -8,14 +9,22 @@ const Dashboard = props => {
             <div className="info"><label>DGS</label><label>Cash</label></div>
             <div className="main-div">
                 <div className="card-status">
-                    <div >card</div>
-                    <div className="pergaminho"></div>
+                    <div className="cards">card</div>
+                    <div className="status">
+                        {/* <div className="pergaminho"> */}
+                        <img src={logo}></img>
+                        <label>Nome</label>
+                        <label>Idade</label>
+                        <label>Gênero</label>
+                        <label>Raça</label>
+                    </div>
                 </div>
                 <div className="btn-battle">
                     <GameButton type="battle" />
                 </div>
             </div>
         </div>
+        // </div>
     )
 }
 
