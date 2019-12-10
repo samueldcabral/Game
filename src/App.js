ceb4dd3ebe1c4543d3aaaa651967885250ad24ac
Adriano Ney Nascimento do Amarala <adriano.amaral147@gmail.com>
2019-12-10 13:11:18 -0300

    algumas alterações do dashboard

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import Create from "./components/Create";
 import Start from "./components/Start";
 import CreateForm from './components/CreateForm';
 import Dashboard from "./components/Dashboard";
-import Battle from './components/Battle';
-import ErrorRoute from "./components/ErrorRoute";
 
 import './App.css';
 import './Style.css';
@@ -20,9 +18,7 @@ function App() {
           <Route path="/create" exact component={Create} />
           <Route path="/create-form" exact component={CreateForm} />
           <Route path="/start" exact component={Start} />
-          <Route path="/battle" exact component={Battle} />
           <Route path="/dashboard/:name" exact component={Dashboard} />
-          <Route path="*" component={ErrorRoute} />
         </Switch>
       </Router>
     </div>
